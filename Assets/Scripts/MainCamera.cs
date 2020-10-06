ee694e714cc6f1fbed502fbe6300d19421292dcf
RaeKim <raekim95@gmail.com>
2020-10-06 18:46:25 +0900

    WASD character movement

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public float cameraMoveSpeed;
+    public Transform playerTransform;
+
+    Vector3 diff;   // 카메라와 캐릭터 사이의 차이 벡터
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diff = playerTransform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = .1f;
+
+    Animator animator;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        dir.Normalize();    // 대각선 이동 감안하여 벡터 길이를 1로 정규화
+
+        bool running = dir.magnitude > 0f;  // 캐릭터 이동
+        animator.SetBool("Running", running);
+
+        if (running)
+        {
+            rb.MovePosition(transform.position + dir * moveSpeed);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
+        
     }
 }
