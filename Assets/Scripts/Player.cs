87e567e4bc9ea10c24a52fc20665aca22c4cacf1
raekim <raekim95@gmail.com>
2020-10-06 13:25:32 +0900

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+    }
+}
