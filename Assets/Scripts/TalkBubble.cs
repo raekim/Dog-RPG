2ef4661e08694e6c4a7bb338046a54a11de6185f
RaeKim <raekim95@gmail.com>
2020-10-22 12:26:13 +0900

    Talk bubble

---
+++ Assets/Scripts/TalkBubble.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TalkBubble : MonoBehaviour
+{
+    public Transform bubbleTransform;
+    float offsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetY = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 위치 조절
+        var pos = Camera.main.WorldToScreenPoint(bubbleTransform.position + Vector3.up * offsetY);
+        transform.position = pos;
+
+        // 투명도 조절
+        //float dist = Vector3.Distance(Camera.main.transform.position, healthBarPositionTransform.position);
+        //if (dist <= 5f) dist = 0f;
+        //ChangeTransparency((transparencyConstant - dist) / transparencyConstant);
+    }
+
+    //void ChangeTransparency(float amount)
+    //{
+    //    transparency = amount;
+    //    foreach (Image im in myImages)
+    //    {
+    //        im.color = new Color(im.color.r, im.color.g, im.color.b, transparency);
+    //    }
+    //    foreach (Text txt in myTexts)
+    //    {
+    //        txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, transparency);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/WordDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WordDisplay : MonoBehaviour
+{
+    public TMP_Text TMP;
+    public float speed;
+    public string words;
+    public bool loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP.text = "";
+        StartCoroutine(display());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator display()
+    {
+        do
+        {
+            TMP.text = "";
+            foreach (char c in words)
+            {
+                TMP.text += c;
+                yield return new WaitForSeconds(speed);
+            }
+            yield return null;
+        }
+        while (loop);
+    }
+}
