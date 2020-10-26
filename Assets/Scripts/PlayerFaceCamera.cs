07b53ff07cba379ee500fa8d3841f5ee121dbb9b
RaeKim <raekim95@gmail.com>
2020-10-26 16:06:47 +0900

    Player health bar camera fixed

---
+++ Assets/Scripts/PlayerFaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFaceCamera : MonoBehaviour
+{
+    public Transform playerModelTransform;
+
+    float dist;
+
+    private void Start()
+    {
+        dist = Vector3.Distance(playerModelTransform.position, transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 캐릭터의 얼굴을 비춘다
+        //transform.position = playerModelTransform.position + playerModelTransform.forward * dist;
+    }
+}
